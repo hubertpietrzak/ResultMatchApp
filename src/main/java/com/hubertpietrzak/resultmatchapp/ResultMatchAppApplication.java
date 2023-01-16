7c274651ad54e039483d63b2ff9fce90c223de80
hubertpietrzak <hubert.pietrzak95@gmail.com>
2023-01-16 15:08:58 +0100

    Creating project

---
+++ src/main/java/com/hubertpietrzak/resultmatchapp/ResultMatchAppApplication.java
@@ -0,0 +1,13 @@
+package com.hubertpietrzak.resultmatchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultMatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultMatchAppApplication.class, args);
+	}
+
+}
